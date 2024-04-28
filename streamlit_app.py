2b5f20cd07328d6c97fa950dcc1776189e0b1eb6
Igor Mossinato <igor.mossinato@gmail.com>
2024-04-28 20:56:11 -0300

    Update streamlit_app.py
    adding api request

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,13 +22,13 @@ ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:',
     my_dataframe)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
 
 if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fdv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
         
     st.write(ingredients_string)
 
